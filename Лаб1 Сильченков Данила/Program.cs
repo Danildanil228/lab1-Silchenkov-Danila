cbc5db1203cad5a94dad602f9a1922ed99f32b85
Danil <d_silchenkov@mail.ru>
2023-09-14 23:41:48 +0200

    Добавьте файлы проекта.

---
+++ Лаб1 Сильченков Данила/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите y: ");
+double y = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите m: ");
+double m = double.Parse(Console.ReadLine());
+Double H = (m * m + 2.8 * m + 0.355) / (Math.Cos(2 * y) + 3.6);
+Console.WriteLine($"H={H:F2}");
