fe3333181169c6f1cae581a67eced98bc6d67282
Danil <134092671+Danildanil228@users.noreply.github.com>
2023-12-08 22:23:21 +0300

    Update Program.cs

--- Лаб1 Сильченков Данила/Program.cs
+++ Лаб1 Сильченков Данила/Program.cs
@@ -1,14 +1,41 @@
-using System.Text;
-
-string pred = Console.ReadLine();
-string[] slow = pred.Split(' ');
-StringBuilder revers = new StringBuilder();
-for (int i = slow.Length -1; i>= 0; i--)
+Console.WriteLine("Введите числа через пробел:");
+try
 {
-    revers.Append(slow[i]);
-    if (i > 0)
+    string input = Console.ReadLine();
+    string[] inputArray = input.Split(' ');
+    int[] array = new int[inputArray.Length];
+    for (int i = 0; i < inputArray.Length; i++)
     {
-        revers.Append(" ");
+        array[i] = int.Parse(inputArray[i]);
+    }
+    int minElement = Min(array);
+    Console.WriteLine($"Минимальный элемент массива: {minElement}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+static int Min(int[] X)
+{
+    if (X.Length == 0)
+    {
+        Console.WriteLine("Массив пуст");
+        return int.MinValue;
+    }
+    else
+    {
+        return Minl(X, 0);
     }
 }
-Console.WriteLine(revers);
+static int Minl(int[] X, int k)
+{
+    if (k == X.Length - 1)
+    {
+        return X[k];
+    }
+    else
+    {
+        int minRest = Minl(X, k + 1);
+        return (X[k] < minRest) ? X[k] : minRest;
+    }
+}
